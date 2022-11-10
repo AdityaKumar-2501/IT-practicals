9380ac128e58349a83cf938343addb11e4195aec
Aditya Kumar <87603870+AdityaKumar-2501@users.noreply.github.com>
2022-11-10 16:01:35 +0530

    prac-8 is not done yet, for prac-7 run it in vscode.

---
+++ Prac-8/index.js
@@ -0,0 +1,18 @@
+function letsChange() {
+    $("table").addClass("tableClass");
+    $("html").addClass("htmlClass");
+    $("#main").addClass("divClass");
+    $("#heading").addClass("heading");
+    $("#fee").addClass("fee");
+    $("th").addClass("th");
+    $("td").addClass("td");
+    $("#criteria").addClass("element criteria");
+    $("#eligibility").addClass("element eligibility");
+    $("#future").addClass("future");
+    $("#futureElement").addClass("futureClass");
+    $("button").hide();
+};
+
+$(function () {
+    // Add , Something you want to execute at the start.
+})
