ef69228d135a13274110b1acb8237202d95ca49d
Aditya Kumar <87603870+AdityaKumar-2501@users.noreply.github.com>
2022-11-10 10:32:00 +0530

    practical 5 and 4 done

---
+++ Prac-5/index.js
@@ -0,0 +1,26 @@
+function change(buttonId) {
+    if (buttonId == 1) {
+        document.getElementById("demoPara").style.color = "red";
+    }
+    if (buttonId == 2) {
+        document.getElementById("demoPara").style.color = "green";
+    } if (buttonId == 3) {
+        document.getElementById("demoPara").style.color = "blue";
+    }
+    if (buttonId == 4) {
+        document.getElementById("demoPara").style.fontSize = "10px";
+    }
+    if (buttonId == 5) {
+        document.getElementById("demoPara").style.fontSize = "20px";
+    } if (buttonId == 6) {
+        document.getElementById("demoPara").style.fontSize = "30px";
+    }
+    if (buttonId == 7) {
+        document.getElementById("demoPara").style.fontFamily = "cursive";
+    }
+    if (buttonId == 8) {
+        document.getElementById("demoPara").style.fontFamily = "Poor Story";
+    } if (buttonId == 9) {
+        document.getElementById("demoPara").style.fontFamily = "Xanh Mono";
+    }
+}
